939cbb5bd9ccf587a13f47023652720c8a5fd3a7
Samodra <samodra.me@gmail.com>
2026-04-14 07:05:13 +0000

    feat: enable global clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,47 @@
 vim.opt.relativenumber = false
 vim.g.autoformat = false
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = function(lines, _)
+      local text = table.concat(lines, "\n")
+      local b64 = vim.fn.system("base64", text):gsub("\n", "")
+      local tty
+      if vim.env.TMUX then
+        tty = vim.fn.system("tmux display-message -p '#{client_tty}'"):gsub("\n", "")
+      else
+        tty = vim.env.SSH_TTY or io.open("/dev/tty", "w")
+      end
+      local f = io.open(tty, "w")
+      if f then
+        f:write("\x1b]52;c;" .. b64 .. "\a")
+        f:close()
+      end
+    end,
+    ["*"] = function(lines, _)
+      local text = table.concat(lines, "\n")
+      local b64 = vim.fn.system("base64", text):gsub("\n", "")
+      local tty
+      if vim.env.TMUX then
+        tty = vim.fn.system("tmux display-message -p '#{client_tty}'"):gsub("\n", "")
+      else
+        tty = vim.env.SSH_TTY or "/dev/tty"
+      end
+      local f = io.open(tty, "w")
+      if f then
+        f:write("\x1b]52;c;" .. b64 .. "\a")
+        f:close()
+      end
+    end,
+  },
+  paste = {
+    ["+"] = function()
+      return { vim.fn.split(vim.fn.getreg('"'), '\n'), vim.fn.getregtype('"') }
+    end,
+    ["*"] = function()
+      return { vim.fn.split(vim.fn.getreg('"'), '\n'), vim.fn.getregtype('"') }
+    end,
+  },
+}
+
