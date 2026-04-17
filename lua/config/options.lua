cbe87dbabc795973da4f2e34caafffa712ba7337
Samodra <samodra.me@gmail.com>
2026-04-17 14:46:35 +0700

    feat: adjust global clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,46 +4,34 @@
 vim.opt.relativenumber = false
 vim.g.autoformat = false
 vim.opt.clipboard = "unnamedplus"
+
+local function osc52_copy(lines)
+  local text = table.concat(lines, "\n")
+  local b64 = vim.base64.encode(text)
+
+  local seq
+  if vim.env.TMUX then
+    seq = "\x1bPtmux;\x1b\x1b]52;c;" .. b64 .. "\x07\x1b\\"
+  else
+    seq = "\x1b]52;c;" .. b64 .. "\x07"
+  end
+
+  io.stdout:write(seq)
+  io.stdout:flush()
+end
+
 vim.g.clipboard = {
-  name = "OSC 52",
+  name = "osc52",
   copy = {
-    ["+"] = function(lines, _)
-      local text = table.concat(lines, "\n")
-      local b64 = vim.fn.system("base64", text):gsub("\n", "")
-      local tty
-      if vim.env.TMUX then
-        tty = vim.fn.system("tmux display-message -p '#{client_tty}'"):gsub("\n", "")
-      else
-        tty = vim.env.SSH_TTY or io.open("/dev/tty", "w")
-      end
-      local f = io.open(tty, "w")
-      if f then
-        f:write("\x1b]52;c;" .. b64 .. "\a")
-        f:close()
-      end
-    end,
-    ["*"] = function(lines, _)
-      local text = table.concat(lines, "\n")
-      local b64 = vim.fn.system("base64", text):gsub("\n", "")
-      local tty
-      if vim.env.TMUX then
-        tty = vim.fn.system("tmux display-message -p '#{client_tty}'"):gsub("\n", "")
-      else
-        tty = vim.env.SSH_TTY or "/dev/tty"
-      end
-      local f = io.open(tty, "w")
-      if f then
-        f:write("\x1b]52;c;" .. b64 .. "\a")
-        f:close()
-      end
-    end,
+    ["+"] = osc52_copy,
+    ["*"] = osc52_copy,
   },
   paste = {
     ["+"] = function()
-      return { vim.fn.split(vim.fn.getreg('"'), '\n'), vim.fn.getregtype('"') }
+      return { vim.fn.split(vim.fn.getreg('"'), "\n"), vim.fn.getregtype('"') }
     end,
     ["*"] = function()
-      return { vim.fn.split(vim.fn.getreg('"'), '\n'), vim.fn.getregtype('"') }
+      return { vim.fn.split(vim.fn.getreg('"'), "\n"), vim.fn.getregtype('"') }
     end,
   },
 }
